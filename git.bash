a78902f46d70b7bd59fa0130963bfe3a0d4ca321
Bryan Rosander <brosander@pentaho.com>
2014-12-04 17:20:26 -0500

    adding git commands, readme

--- git.bash
+++ git.bash
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# Shorthand for git status -s
 alias gss='git status -s'
+
+# Pushes current branch to origin
 alias gpo='git status | grep "On branch " | awk "{print \$NF}" | xargs git push origin'
+
+# Checks out master, merges in upstream/master, and pushes to origin
 alias gum='git checkout master && git fetch upstream && git merge upstream/master && git push origin master'
+
+# Shows all files that match gss grep
+function gsg {
+  gss | grep "$1" | awk '{print $NF}'
+}
+
+# Runs git command on all files from gsg
+function gbc {
+  gsg "$1" | xargs git "$2"
+}
