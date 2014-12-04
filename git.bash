eff7b172c5d143b85fa267e49d49cf0ff5970731
Bryan Rosander <brosander@pentaho.com>
2014-12-04 12:40:35 -0500

    making gpo push current branch

--- git.bash
+++ git.bash
@@ -1,3 +1,3 @@
 alias gss='git status -s'
-alias gpo='git push origin'
+alias gpo='git status | grep "On branch " | awk "{print \$NF}" | xargs git push origin'
 alias gum='git checkout master && git fetch upstream && git merge upstream/master && git push origin master'
