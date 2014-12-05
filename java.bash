4e0b144ce307c7c9836ce719545f04a259b3f6af
Bryan Rosander <brosander@pentaho.com>
2014-12-05 13:41:01 -0500

    adding util functions to add license headers to all java files in a project

---
+++ java.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This function will add the license header in file $1 to every java file
+function addLicenseJava {
+  echo "addLicense \"$1\""
+  find . -name '*.java' -type f -print0 | xargs -0 -I {} bash -c "source ~/.bashrc && addLicenseJavaIndividual '$1' '{}'"
+}
+
+# This function will add the license header in file $1 to file $2
+function addLicenseJavaIndividual {
+  echo "addLicenseJavaIndividual \"$1\" \"$2\""
+  local tmpFile=$(mktemp)
+  local packageLineNo="`grep -n '^package ' \"$2\" | sed 's/^\([0-9]\+\).*$/\1/g'`"
+  local tailCommand="tail -n+$packageLineNo \"$2\" >> \"$tmpFile\""
+  #echo "$tailCommand"
+  cat "$1" > "$tmpFile" && eval "$tailCommand" && mv "$tmpFile" "$2"
+}
