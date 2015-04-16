3b51b2f1a61d0539628777ffa658ee65190568c3
Bryan Rosander <brosander@pentaho.com>
2015-04-16 11:12:39 -0400

    updated license functionality

--- license.bash
+++ license.bash
@@ -6,6 +6,11 @@ function addLicenseJava {
   find . -name '*.java' -type f -print0 | xargs -0 -I {} bash -c "source ~/.bashrc && addLicenseJavaIndividual '$1' '{}'"
 }
 
+function addLicenseTopExtension {
+  echo "addLicenseJS \"$1\""
+  find . -name "*.$2" -type f -print0 | xargs -0 -I {} bash -c "source ~/.bashrc && addLicenseTopOfFile '$1' '{}'"
+}
+
 # This function will add the license header in file $1 to file $2
 function addLicenseJavaIndividual {
   echo "addLicenseJavaIndividual \"$1\" \"$2\""
@@ -14,4 +19,10 @@ function addLicenseJavaIndividual {
   local tailCommand="tail -n+$packageLineNo \"$2\" >> \"$tmpFile\""
   #echo "$tailCommand"
   cat "$1" > "$tmpFile" && eval "$tailCommand" && mv "$tmpFile" "$2"
+}
+
+function addLicenseTopOfFile {
+  echo "addLicenseTopOfFile \"$1\" \"$2\""
+  local tmpFile=$(mktemp)
+  cat "$1" > "$tmpFile" && cat "$2" >> "$tmpFile" && mv "$tmpFile" "$2"
 }
