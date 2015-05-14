7b862bd7a35392c7e76f3fd483b8f161ff9a525d
Bryan Rosander <brosander@pentaho.com>
2015-05-14 13:24:21 -0400

    Adding optional file filtering based on arguments

--- python/checkstyle.py
+++ python/checkstyle.py
@@ -2,29 +2,35 @@
 
 import os
 import xml.etree.ElementTree as ET
+import sys
 
-def find_violations_file(rootDir, filename):
+def find_violations_file(rootDir, filename, fileFilter):
   tree = ET.parse(filename)
   results = []
   for parentFile in tree.getroot().iter('file'):
     errors = []
-    for error in parentFile.iter('error'):
-      errors.append({'line': error.get('line'), 'message': error.get('message'), 'column': error.get('column')})
-    if len(errors) > 0:
-      results.append({'filename': os.path.relpath(parentFile.get('name'), rootDir), 'errors': errors})
+    relName = os.path.relpath(parentFile.get('name'), rootDir)
+    if fileFilter is None or relName in fileFilter:
+      for error in parentFile.iter('error'):
+        errors.append({'line': error.get('line'), 'message': error.get('message'), 'column': error.get('column')})
+      if len(errors) > 0:
+        results.append({'filename': relName, 'errors': errors})
   return results
 
-def find_violations_recursively(rootDir):
+def find_violations_recursively(rootDir, fileFilter):
   results = []
   rootDir =  os.path.abspath(rootDir)
   for root, dirs, files in os.walk(rootDir):
     if 'checkstyle-result.xml' in files:
       filename = os.path.join(root, 'checkstyle-result.xml')
-      results.extend(find_violations_file(rootDir, filename))
+      results.extend(find_violations_file(rootDir, filename, fileFilter))
   return results
 
 if __name__ == '__main__':
-  for errorFile in find_violations_recursively(os.getcwd()):
+  fileFilter = None
+  if len(sys.argv) > 1:
+    fileFilter = set([filename for filename in sys.argv[1:]])
+  for errorFile in find_violations_recursively(os.getcwd(), fileFilter):
     print(errorFile['filename'])
     for error in errorFile['errors']:
       print('  line: ' + str(error['line']) + ' column: ' + str(error['column']) + ' message: ' + str(error['message']))
