c539c1d00a846dec4e349f3b46fdb2f2d6d6922c
Bryan Rosander <brosander@pentaho.com>
2015-06-04 14:34:11 -0400

    adding prcheck function

--- python.bash
+++ python.bash
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 export PATH="$PATH:`dirname ${BASH_SOURCE[0]}`/python"
+
+prcheck() {
+  if [ -z "$1" ]; then
+    COMMIT="HEAD~1"
+  else
+    COMMIT="$1"
+  fi
+  git diff --stat=10000 --relative "$COMMIT" | head -n-1 | awk '{print $1}' | xargs checkstyle.py
+}
