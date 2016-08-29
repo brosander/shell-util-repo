8b47a14478fccc9c720777bbeacdb28604ac5603
Bryan Rosander <bryanrosander@gmail.com>
2016-08-29 13:54:16 -0400

    adding function for easily finding and editing a file by name

---
+++ shell.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function fe() {
+  if [ -z "$1" ]; then
+    echo "Usage: fe [DIR_ARG] NAME_ARG"
+    echo "    Runs a find command and then invokes \$EDITOR on the first line of the output."
+  else
+    if [ -z "$2" ]; then
+      local NAME_ARG="$1"
+      local DIR_ARG="./"
+    else
+      local NAME_ARG="$2"
+      local DIR_ARG="$1"
+    fi
+    local FILE_TO_EDIT="$(find "$DIR_ARG" -name "$NAME_ARG" | head -n 1)"
+    if [ -z "$FILE_TO_EDIT" ]; then
+      echo "Unable to find file matching '$NAME_ARG' in dir '$DIR_ARG'"
+    else
+      if [ -z "$EDITOR" ]; then
+        local EDITOR="vi"
+      fi
+      echo "$EDITOR" "$FILE_TO_EDIT"
+      "$EDITOR" "$FILE_TO_EDIT"
+    fi
+  fi
+}
